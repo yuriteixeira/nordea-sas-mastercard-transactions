7e138d54656259fee7c1e4bb5c0ae4b6cadafcf4
Yuri Teixeira <oyuriteixeira@gmail.com>
2018-10-27 21:51:47 +0200

    Fix CORs header filters

--- bin/api.js
+++ bin/api.js
@@ -34,6 +34,6 @@ async function sendTransactionsToYnab(req, res) {
 
 function cors (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 }
