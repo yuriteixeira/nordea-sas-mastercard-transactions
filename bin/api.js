f6773bc4293463379c3fa952f06d9d146e6cce3f
Yuri Teixeira <oyuriteixeira@gmail.com>
2019-01-01 22:36:13 +0100

    Remove dull space

--- bin/api.js
+++ bin/api.js
@@ -76,7 +76,7 @@ async function sendTransactionsToYnab(req, res) {
   }
 }
 
-function cors (req, res, next) {
+function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
